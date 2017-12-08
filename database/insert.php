fd786091b75e63288db08539ccf80f037a62d01f
xyz607xx <xyz607xx@gmail.com>
2017-12-08 22:59:59 +0800

    僅剩下value使用bindValue

--- database/insert.php
+++ database/insert.php
@@ -3,10 +3,7 @@
     $value = join(",",$_POST['data']);
     $tableName=$_POST['table'];
     include_once("./connect.php");
-    $result=$db->prepare("INSERT INTO :tableName(:title) VALUES (:value)");
-	$result->bindValue('tableName',$tableName);
-    $result->bindValue('title',$title);
+    $result=$db->prepare("INSERT INTO $tableName($title) VALUES (:value)");
     $result->bindValue('value',$value);
 	$result->execute();
-    // var_dump($result);
 ?>
