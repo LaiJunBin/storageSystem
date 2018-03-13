ff0083acc5d489807205575c5fd15e1abae09433
xyz607xx <xyz607xx@gmail.com>
2018-03-13 09:45:21 +0800

    建立資料表

---
+++ database/migrations/2018_03_13_014239_create_class_name_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassNameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_name', function (Blueprint $table) {
+            $table->string('id')->primary()->uniqid();
+            $table->string('class_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_name');
+    }
+}
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="footer-copyright py-3 text-center">
         由資料處理科賴俊賓製作
         <span><a class="github-button" href="https://github.com/xyz607xx" aria-label="Follow @xyz607xx on GitHub">GitHub</a></span>
-        <span><a class="github-button" href="https://github.com/xyz607xx/library_lend" aria-label="Watch ntkme/github-buttons on GitHub">Watch</a></span>
+        <span><a class="github-button" href="https://github.com/xyz607xx/storagesystem" aria-label="Watch ntkme/github-buttons on GitHub">Watch</a></span>
         <script src="https://buttons.github.io/buttons.js"></script>
         <script>
             $(function(){
