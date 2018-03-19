9d0adbf18d77521f29a0c1698d7ac369542932e8
xyz607xx <xyz607xx@gmail.com>
2018-03-19 08:41:15 +0800

    修正錯字

--- app/Services/BindingService.php
+++ app/Services/BindingService.php
@@ -12,7 +12,7 @@ class BindingService
             $user_result = User::where(['email'=>session('user_email')])->firstOrFail();
             if($user_result->type == 'A'){
                 $binding['dropItems']['manager'] = ['url'=>url('managerClass'),'title'=>'管理教室'];
-                $binding['dropItems']['verificationUser'] = ['url'=>url('verificationUser'),'title'=>'驗用帳戶'];
+                $binding['dropItems']['verificationUser'] = ['url'=>url('verificationUser'),'title'=>'驗證帳戶'];
                 $binding['dropItems']['addMaterial'] = ['url'=>url('material/manager'),'title'=>'管理材料'];
             }else{
                 $binding['dropItems']['material'] = ['url'=>url('material'),'title'=>'材料申請'];
