3083b79a0f3c5bdce4a0c97c944ac36f357857b0
xyz607xx <xyz607xx@gmail.com>
2018-03-20 10:26:24 +0800

    更改寫法

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\BindingService;
 use App\ClassName;
+Use App\Stock;
 
 class MainController extends Controller
 {
@@ -12,7 +13,14 @@ class MainController extends Controller
         $binding = BindingService::binding();
         $className_result = ClassName::get();
         $binding['classNames'] = $className_result->pluck('class_name')->toarray();
-        $binding['classNumbers'] = $className_result->pluck('id')->toarray();
+        foreach ($binding['classNames'] as $className) {
+            $binding['classStock'][$className] = Stock::where('class_name',$className)->get()->toarray();
+            $binding['classStock'][$className] = array_map(function($x){
+                $x['item'] = unserialize($x['item']);
+                return $x;
+            },$binding['classStock'][$className]);
+        }
+        dd($binding);
         return view('index',$binding);
     }
 }
